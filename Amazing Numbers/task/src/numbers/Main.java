498c5377e416c07cba5022a056b6f42e2dfabe0b
Work <yakushevso@ya.ru>
2024-03-21 08:03:00 +0300

    Stage 2/8: Duck numbers

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -4,19 +4,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Enter a natural number:");
-            int num = sc.nextInt();
+            int num = scanner.nextInt();
 
             if (isnNaturalNumbers(num)) {
-                System.out.println(isOdd(num) ? "This number is Odd." : "This number is Even.");
-                System.out.println(isBuzzNumber(num) ? "It is a Buzz number." : "It is not a Buzz number.");
+                System.out.printf("""
+                                Properties of %d
+                                        even: %b
+                                         odd: %b
+                                        buzz: %b
+                                        duck: %b""",
+                        num, !isOdd(num), isOdd(num),
+                        isBuzzNumber(num), isDuck(num));
             } else {
                 System.out.println("This number is not natural!");
             }
-
-            printExplanationBuzz(num);
         }
+    }
+
+    private static boolean isDuck(int num) {
+        int numTrim = num;
+        for (int i = 10; i <= num; i *= 10) {
+            if (numTrim % 10 == 0) {
+                return true;
+            }
+            numTrim /= 10;
+        }
+        return false;
     }
 
     private static boolean isnNaturalNumbers(int num) {
@@ -29,19 +44,5 @@ public class Main {
 
     private static boolean isBuzzNumber(int num) {
         return num % 7 == 0 || num % 10 == 7;
-    }
-
-    private static void printExplanationBuzz(int num) {
-        System.out.println("Explanation:");
-
-        if (num % 7 == 0 && num % 10 == 7) {
-            System.out.println(num + " is divisible by 7 and ends with 7");
-        } else if (num % 7 == 0) {
-            System.out.println(num + " is divisible by 7");
-        } else if (num % 10 == 7) {
-            System.out.println(num + " ends with 7");
-        } else {
-            System.out.println(num + " is neither divisible by 7 nor does it end with 7");
-        }
     }
 }
