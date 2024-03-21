8ec68cec47fcb0a6b919e3cfb96db08878cf56d3
Work <yakushevso@ya.ru>
2024-03-21 08:17:01 +0300

    Stage 3/8: Palindromic numbers

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -1,48 +1,82 @@
 package numbers;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Enter a natural number:");
-            int num = scanner.nextInt();
+            System.out.println("Welcome to Amazing Numbers!");
+            instructions();
 
-            if (isnNaturalNumbers(num)) {
-                System.out.printf("""
-                                Properties of %d
-                                        even: %b
-                                         odd: %b
-                                        buzz: %b
-                                        duck: %b""",
-                        num, !isOdd(num), isOdd(num),
-                        isBuzzNumber(num), isDuck(num));
-            } else {
-                System.out.println("This number is not natural!");
+            while (true) {
+                System.out.println("Enter a request:");
+                long num = scanner.nextLong();
+
+                if (isnNaturalNumbers(num)) {
+                    System.out.printf(Locale.US, """
+                                    Properties of %,d
+                                            even: %b
+                                             odd: %b
+                                            buzz: %b
+                                            duck: %b
+                                     palindromic: %b
+                                    """,
+                            num, !isOdd(num), isOdd(num),
+                            isBuzzNumber(num), isDuck(num),
+                            palindromic(num));
+                } else if (num == 0) {
+                    System.out.println("Goodbye!");
+                    break;
+                } else {
+                    System.out.println("The first parameter should be a natural number or zero.");
+                }
             }
         }
     }
 
-    private static boolean isDuck(int num) {
-        int numTrim = num;
-        for (int i = 10; i <= num; i *= 10) {
+    private static boolean palindromic(long num) {
+        long numSrc = num;
+        long reversed = 0;
+
+        while (numSrc != 0) {
+            reversed = reversed * 10 + (numSrc % 10);
+            numSrc /= 10;
+        }
+
+        return num == reversed;
+    }
+
+    private static void instructions() {
+        System.out.println("""
+                Supported requests:
+                - enter a natural number to know its properties;
+                - enter 0 to exit.""");
+    }
+
+    private static boolean isDuck(long num) {
+        long numTrim = num;
+
+        for (long i = numTrim; i >= 10; i /= 10) {
             if (numTrim % 10 == 0) {
                 return true;
             }
+
             numTrim /= 10;
         }
+
         return false;
     }
 
-    private static boolean isnNaturalNumbers(int num) {
-        return num > 0;
+    private static boolean isBuzzNumber(long num) {
+        return num % 7 == 0 || num % 10 == 7;
     }
 
-    private static boolean isOdd(int num) {
+    private static boolean isOdd(long num) {
         return (num & 1) == 1;
     }
 
-    private static boolean isBuzzNumber(int num) {
-        return num % 7 == 0 || num % 10 == 7;
+    private static boolean isnNaturalNumbers(long num) {
+        return num > 0;
     }
 }
